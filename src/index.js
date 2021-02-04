3819b1f83b5ee4e5ec53d8a1d5b5b744902d30da
HyeokMin <hyeokmin22@gmail.com>
2021-02-04 18:03:47 +0900

    react-redux async init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import rootReducer from './modules';
+// import loggerMiddleWare from './lib/loggerMiddleWare';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 import reportWebVitals from './reportWebVitals';
+
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
